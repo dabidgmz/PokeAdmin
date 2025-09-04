77a8d8389ea7f0feecfe0cb3b109a3b635fc74f1
David Herrera <133131368+dabidgmz@users.noreply.github.com>
2025-09-04 11:28:04 -0600

    Add PWA support with service worker and manifest
    Integrated Angular service worker for PWA capabilities by adding @angular/service-worker, ngsw-config.json, and updating app configuration. Added web app manifest and icon assets for installability. Updated index.html to reference the manifest and improved service worker registration in main.ts.

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,16 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
+
+if('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/ngsw-worker.js').then(() => {
+      console.log('Service Worker registrado con éxito yujujuuu');
+    }).catch((err) => {
+      console.error('Service Worker no registrado buu', err);
+    });
+  });
+}
+
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
